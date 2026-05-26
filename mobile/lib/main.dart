44702dc47b55b7d35aec857f25b3d7a7745b39f5
Sophie Ren <sophier4@uci.edu>
2026-05-25 18:42:35 -0700

    Modified UI structure: AppBar and BottomBar are all under main_dashboard now

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bathtub_models/presentation/screens/main_dashboard.dart';
+import 'presentation/screens/main_dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- mobile/lib/presentation/screens/data_screen.dart
+++ mobile/lib/presentation/screens/data_screen.dart
@@ -1,56 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class DataScreen extends StatelessWidget {
   const DataScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 239, 242, 247),
-      appBar: AppBar(
-        title: Text(
-          'Data',
-          style: GoogleFonts.electrolize(
-            color: Theme.of(context).colorScheme.primary,
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            elevation: 4,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                '1. For each individual: Collect their trip remaining time\n'
+                'If the destination is within the bathtub area, remaining time = time needed to end the trip.\n'
+                'If the destination is outside the bathtub area, remaining time = time needed to exit the bathtub area.',
+              ),
+            ),
           ),
-        ),
-      ),
-
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(
-              elevation: 4,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  '1. For each individual: Collect their trip remaining time\n'
-                  'If the destination is within the bathtub area, remaining time = time needed to end the trip.\n'
-                  'If the destination is outside the bathtub area, remaining time = time needed to exit the bathtub area.',
-                ),
+          SizedBox(height: 16),
+          Card(
+            elevation: 4,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                '2. For the overall bathtub model:\n'
+                'At time t (i.e. right now):\n'
+                '(1) How many cars have already entered before time t?\n'
+                '(2) How many cars are entering at time t?\n'
+                '(3) How many cars are leaving at time t?',
               ),
             ),
-            SizedBox(height: 16),
-            Card(
-              elevation: 4,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  '2. For the overall bathtub model:\n'
-                  'At time t (i.e. right now):\n'
-                  '(1) How many cars have already entered before time t?\n'
-                  '(2) How many cars are entering at time t?\n'
-                  '(3) How many cars are leaving at time t?',
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
